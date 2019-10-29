d2bbe22c8c1e53b39de7620b9ecc2bb4a7ca358a
AimotoRyosuke <r.aimoto97vg@gmail.com>
2019-10-29 19:40:20 +0900

    ajax-message3

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,19 +1,6 @@
 $(function(){
   function appendMessage(data){
-    if(data.image.url === null){
-      let message =  `<li class="message-list__message">
-                          <p class="message-list__message__user">
-                            ${data.user}
-                          </p>
-                          <p class="message-list__message__date">
-                            ${data.date}
-                          </p>
-                          <p class="message-list__message__text">
-                            ${data.body}
-                          </p>
-                        </li>`
-                        return message;
-    } else {
+      let img_tag = (data.image.url ) ? `<img src="${data.image.url}">` : ("");
       let message =  `<li class="message-list__message">
                         <p class="message-list__message__user">
                           ${data.user}
@@ -24,13 +11,11 @@ $(function(){
                         <p class="message-list__message__text">
                           ${data.body}
                         </p>
-                        <img src="${data.image.url}">
+                        ${img_tag}
                       </li>`
       return message;
-    }
+    
   }
-
-  // for(i = 0 ; i < 1 ; i++){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -46,10 +31,9 @@ $(function(){
       .done(function(data){
         let new_message = appendMessage(data);
         $('.message-list').append(new_message)
-        $('.send-box__form__input-form').val('')
-        $('.send-box__form__file-select').val('')
+        $('#new_message')[0].reset();
         $('.send-box__form__submit-btn').removeAttr('disabled');
-        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight})
       })
       .fail(function(){
         alert('メッセージを入力してください');
--- chat-space/app/views/messages/create.json.jbuilder
+++ chat-space/app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
-json.id @message.id
-json.user @message.user.name
-json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
-json.body @message.body
+json.id    @message.id
+json.user  @message.user.name
+json.date  @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.body  @message.body
 json.image @message.image
