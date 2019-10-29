cdad2200009261177db6019dc321deb544046d80
AimotoRyosuke <r.aimoto97vg@gmail.com>
2019-10-29 18:00:52 +0900

    ajax-message2

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -9,14 +9,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.create(message_params)
-    # if @message.save
-    #   redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    # else
-    #   @messages =@group.messages.includes(:user)
-    #   # binding.pry
-    #   flash.now[:alert] = 'メッセージを入力してください。'
-    #   render :index
-    # end
     respond_to do |format|
       format.html { redirect_to group_messages_path(params[:group_id])}
       format.json {@message}
--- chat-space/app/views/messages/create.json.jbuilder
+++ chat-space/app/views/messages/create.json.jbuilder
@@ -3,4 +3,3 @@ json.user @message.user.name
 json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.body @message.body
 json.image @message.image
-# binding.pry
