23278556fb025c0df7b6e0ff920ee994189b419c
AimotoRyosuke <r.aimoto97vg@gmail.com>
2019-11-19 16:55:16 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module ChatSpace
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
   end
 end
